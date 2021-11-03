6dc296443005c5dea5f6d3515be9c877bf5353a7
sairam1411 <majgaonkars3@gmail.com>
2021-11-03 18:52:33 +0530

    Rebrand to DFC Kernel
    Dont want crybabies claming common kernel names
    
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Lightning Kernel by Sairam60
+kernel.string=DFC Kernel by Sairam60
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
