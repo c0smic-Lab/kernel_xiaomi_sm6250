c1bc9dd4e70069d17b663c12cfa74bb1b94b3281
COSMIC <rayavanindra@gmail.com>
2024-07-20 00:22:57 +0530

    AK3: Update AnyKernel3 script and tools

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -1,12 +1,13 @@
-# AnyKernel3 Ramdisk Mod Script
-# osm0sis @ xda-developers
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
-## AnyKernel setup
-# begin properties
+### AnyKernel setup
+# global properties
 properties() { '
 kernel.string=DFC Kernel by Sairam60
 do.devicecheck=1
 do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=miatoll
@@ -14,35 +15,25 @@ device.name2=curtana
 device.name3=excalibur
 device.name4=gram
 device.name5=joyeuse
-supported.versions=11.0-12.0
+supported.versions=11.0-14.0
+supported.patchlevels=
+supported.vendorpatchlevels=
 '; } # end properties
 
-# shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=auto;
-ramdisk_compression=auto;
+### AnyKernel install
+# boot shell variables
+BLOCK=/dev/block/bootdevice/by-name/boot;
+IS_SLOT_DEVICE=auto;
+RAMDISK_COMPRESSION=none;
+PATCH_VBMETA_FLAG=auto;
+NO_BLOCK_DISPLAY=1
 
-
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
+# boot install
+split_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 750 750 $ramdisk/*;
-
-
-## AnyKernel install
-dump_boot;
-
-# Begin Ramdisk Changes
-
-# migrate from /overlay to /overlay.d to enable SAR Magisk
-if [ -d $ramdisk/overlay ]; then
-  rm -rf $ramdisk/overlay;
-fi;
-
-write_boot;
-## end install
-
+flash_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+flash_dtbo;
+## end boot install
